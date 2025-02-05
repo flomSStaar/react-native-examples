1c1fb3b77ddd13e8b3526e76f07ef1f5836e9a5b
Florent MARQUES <f.marques@overscan.fr>
2025-02-05 10:34:02 +0100

    :lipstick: increase clickable area on floating tab bar

--- apps/floating-bottom-bar/components/TabButton.tsx
+++ apps/floating-bottom-bar/components/TabButton.tsx
@@ -4,6 +4,7 @@ import { forwardRef, Ref } from 'react'
 import { Pressable, View } from 'react-native'
 
 import { Borders } from '@/constants/Borders'
+import { Spacing } from '@/constants/Spacing'
 import { useThemeColor } from '@/hooks/useThemeColor'
 
 export type TabButtonProps = TabTriggerSlotProps & {
@@ -20,14 +21,24 @@ export const TabButton = forwardRef(({ icon, children, isFocused, ...props }: Ta
       {...props}
       style={[
         {
+          padding: Spacing.sm,
+          margin: -Spacing.sm,
           borderRadius: Borders.radius.full,
-          aspectRatio: 1,
-          padding: 8,
         },
-        { backgroundColor: isFocused ? colors.tintColor : colors.surface },
       ]}
     >
-      <TabIcon size={28} color={isFocused ? colors.surface : colors.tintColor} />
+      <View
+        style={[
+          {
+            borderRadius: Borders.radius.full,
+            aspectRatio: 1,
+            padding: Spacing.sm,
+          },
+          { backgroundColor: isFocused ? colors.tintColor : colors.surface },
+        ]}
+      >
+        <TabIcon size={28} color={isFocused ? colors.surface : colors.tintColor} />
+      </View>
     </Pressable>
   )
 })
